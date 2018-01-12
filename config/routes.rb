7be1da85adbcae3fe1819fe5896bf56dd641dde2
Konrad Pason <konrad.pason17@gmail.com>
2018-01-12 12:11:21 +0100

    Update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'users/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations"}
 
 
   root 'welcome#index'
