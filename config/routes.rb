afb0f3baa9e78cc4b3b7bd425a355915f41dd5d7
Konrad Pason <konrad.pason17@gmail.com>
2018-01-17 15:59:41 +0100

    Add destroy action and modify views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'friendships/destroy'
+
+  get 'friendships_controller/destroy'
+
   get 'users/index'
 
   devise_for :users, :controllers => { :registrations => "user/registrations"}
